d047b084fcd09066edda674c475bac3ad135c604
Ubuntu <ubuntu@ip-172-31-17-38.eu-north-1.compute.internal>
2023-09-18 18:53:33 +0000

    set DEBUG as False

--- technohub/technohub/settings.py
+++ technohub/technohub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#bool(int(os.environ.get("DEBUG", 0)))
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0'] if DEBUG else os.environ.get("ALLOWED_HOSTS").split(',')
 
